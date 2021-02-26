c39752ebd4b61895b9a0e0e84903be005b9f2aa5
KaO <52933590+painteau@users.noreply.github.com>
2021-02-26 12:34:23 +0100

    Update 2.Launch at Windows Start.bat

--- 2.Launch at Windows Start.bat
+++ 2.Launch at Windows Start.bat
@@ -1,4 +1,14 @@
-DEL /Q C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\PhoenixMiner.lnk
+@echo off > NUL
 cls
-copy "PhoenixMiner.lnk" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\"
+echo 1. Install "launching mining at Windows Start"
+echo 2. Remove "launching mining at Windows Start"
+set /p id=Enter your choice and press Enter to validate :
+
+IF %id%==1 (
+  DEL /Q C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\PhoenixMiner.lnk
+  cls
+  copy "PhoenixMiner.lnk" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\"
+) ELSE IF %id%==2 (
+  DEL /Q C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\PhoenixMiner.lnk
+)
 pause
