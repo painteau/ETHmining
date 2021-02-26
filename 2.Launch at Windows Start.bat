123e893dd321b799d8a2244dfb6a82eb497dff28
KaO <52933590+painteau@users.noreply.github.com>
2021-02-26 12:25:42 +0100

    Update 2.Launch at Windows Start.bat

--- 2.Launch at Windows Start.bat
+++ 2.Launch at Windows Start.bat
@@ -1,2 +1,4 @@
+DEL /Q C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\PhoenixMiner.lnk
+cls
 copy "PhoenixMiner.lnk" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\"
 pause
