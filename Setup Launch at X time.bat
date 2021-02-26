565d954b20a2776360777afee12b53bdd08e8656
KaO <52933590+painteau@users.noreply.github.com>
2021-02-26 12:56:36 +0100

    Update Setup Launch at X time.bat

--- Setup Launch at X time.bat
+++ Setup Launch at X time.bat
@@ -2,18 +2,22 @@
 cls
 echo 1. Install "launching mining at X time"
 echo 2. Remove "launching mining at X time"
-set /p id=Enter your choice and press Enter to validate :
+set /p iid=Enter your choice and press Enter to validate : 
 cls
-IF %id%==1 (
+IF %iid%==1 (GOTO install)
+IF %iid%==2 (GOTO uninstall)
+GOTO end
+
+:install
 echo Choose your time for automatic launch (24:00 format, ex : 02:00 for 2AM or 14:00 for 2PM)
-set /p tiime=Enter your choice and press Enter to validate :
+set /p tiime=Enter your choice and press Enter to validate : 
 SCHTASKS /CREATE /SC DAILY /TN "LaunchMining" /TR "C:\mining\GOGO.bat" /ST %tiime%
+GOTO end
 
-) ELSE IF %id%==2 (
+:uninstall
+cls
 SCHTASKS /DELETE /TN "LaunchMining"
-)
+GOTO end
+
+:end
 pause
-
-
-
-
